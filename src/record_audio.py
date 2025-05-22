171271f88f8df3ad0308149c9b3d76df4f0ebf31
adulegendary <adonai31.edu@gmail.com>
2025-05-21 22:42:27 -0700

    sample recoding using window

--- src/record_audio.py
+++ src/record_audio.py
@@ -1,2 +1,18 @@
+# record_sample.py
+
 import sounddevice as sd
-audio = sd.rec(...)# record a short audio clip
+import soundfile as sf
+
+print(sd.query_devices())
+print("it getting executed")
+# def record(filename, duration=5):
+#     fs = 16000  # 16 kHz sample rate
+#     print(f"🎤 Recording '{filename}' for {duration} seconds...")
+#     audio = sd.rec(int(duration * fs), samplerate=fs, channels=1)
+#     sd.wait()
+#     sf.write(filename, audio, fs)
+#     print("✅ Recording saved to:", filename)
+#
+#
+# # EXAMPLE USAGE:
+# record("data/voices/priest.wav", duration=5)
